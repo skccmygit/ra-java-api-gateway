f56979730c9d02f58c9c868b9fcf630557c9c1ed
skcc-tom01 <20073917@skcc.com>
2025-04-02 16:15:35 +0700

    remove redis config

--- api-gateway/src/main/java/kr/co/skcc/base/gateway/domain/Token.java
+++ api-gateway/src/main/java/kr/co/skcc/base/gateway/domain/Token.java
@@ -2,21 +2,16 @@ package kr.co.skcc.base.gateway.domain;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.redis.core.RedisHash;
-import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.Instant;
 
 @Data
-@RedisHash("RefreshToken")
 public class Token {
     @Id
     private String userid;
 
-    @Indexed
     private String accessToken;
 
-    @Indexed
     private String oldAccessToken;
 
     private String refreshToken;
