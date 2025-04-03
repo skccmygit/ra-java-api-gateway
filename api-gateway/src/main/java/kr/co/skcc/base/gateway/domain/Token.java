3f6aea44aa19a50fbfbf032c16b6ea8998d3313b
skcc-tom01 <20073917@skcc.com>
2025-04-03 15:26:49 +0700

    revert code

--- api-gateway/src/main/java/kr/co/skcc/base/gateway/domain/Token.java
+++ api-gateway/src/main/java/kr/co/skcc/base/gateway/domain/Token.java
@@ -2,16 +2,21 @@ package kr.co.skcc.base.gateway.domain;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.index.Indexed;
 
 import java.time.Instant;
 
 @Data
+@RedisHash("RefreshToken")
 public class Token {
     @Id
     private String userid;
 
+    @Indexed
     private String accessToken;
 
+    @Indexed
     private String oldAccessToken;
 
     private String refreshToken;
